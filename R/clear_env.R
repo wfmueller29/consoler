a04cd130c57ca8d25195b20fa67730c87a9d44e7
Billy Mueller <muellerwf@nih.gov>
2022-04-06 16:22:57 -0400

    exported commands

--- R/clear_env.R
+++ R/clear_env.R
@@ -3,6 +3,8 @@
 #' Remove all objects from global environment default is global env
 #' 
 #' @param env the enironmen to clear
+#'
+#' @export
 clear_env <- function(env = .GlobalEnv) {
   rm(list = ls(env), envir = env)
 }
